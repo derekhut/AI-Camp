7266165b24f44952b8a12c9a881fb951757038a1
Derek Hu <tong.hu@sloan.mit.edu>
2025-03-08 22:31:02 +0800

    Update branding section: adjusted title font size and position

--- components/blocks/branding/index.tsx
+++ components/blocks/branding/index.tsx
@@ -6,11 +6,11 @@ export default function Branding({ section }: { section: SectionType }) {
   }
 
   return (
-    <section id={section.name} className="py-16">
+    <section id={section.name} className="py-16 mt-32">
       <div className="container flex flex-row items-center justify-center">
         <div className="flex flex-col items-center gap-4">
-          <h2 className="text-center: text-muted-foreground lg:text-left">
-            {section.title}
+          <h2 className="text-center text-xl font-bold lg:text-2xl mb-6 text-balance">
+            <span className="bg-gradient-to-r from-primary via-primary to-primary bg-clip-text text-transparent">{section.title}</span>
           </h2>
           <div className="flex flex-wrap items-center justify-center gap-8 mt-4">
             {section.items?.map((item, idx) => {
